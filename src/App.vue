7fa6021b0e3b9389353a43b103431024a6ec7267
hellolin-oi <hellolinoi@outlook.com>
2024-02-13 13:49:02 +0800

    feat: Show some info as dialogs

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,60 @@ setColorScheme('#656d88');
 </script>
 
 <template lang="pug">
+mdui-dialog.pgp_dialog(headline='My PGP Public Key' description='6D27 69C0 6088 2947' close-on-overlay-click)
+  pre.
+    -----BEGIN PGP PUBLIC KEY BLOCK-----
+
+    mQINBGXJ7WwBEAC6MqHdHHU/dilzZk80KMznuaN4c6Ytrvg48FuFElAcmS2/unQq
+    86Wc8f25CWILCMsi4DkSyutFzVwNR90vHwa3OpfWu7+96ApyEUUHBDFBovHCNNRr
+    8E2Vb4To7K5Q8pGzLCKUaCWVzheCl0Ri5m9k6QsaPqHN3R7o6+mItAyiPdnuB8QO
+    uMjI7VdeV+kEaFWy3aOSSg1dLgyQ8UVBAT8XFeN5shs2JwtO43cXrzoA7mrwfrXI
+    MO/59SYj+oIMmB5K28np2rYGhtXHOROOMAWuakDgOTefiy4+eh6NdpY5zngKCga2
+    r0GuDD2W8yB+O1MQOEQSZLwEfBeFOX7wD5tRa+83oKmI+0UZIKKtQrXPhAo/foqf
+    vAdcJ07WytR4xb6lFX3Foc9cFsUoRyd3QRDiT74VUeGWnXDA3OSAE13fXU0tZVH8
+    CrBMwX3cHN2PuSbrsWcpCZ/bRSFbl54t/9Ehla5LHyMPopj8p5VwNpr/Goho96dE
+    K0m/80ksz6W/7/UnnJyHOnu3dQYOkHSh4kVET1OPgI0yk+VKgNmKvfl8joFRPV1a
+    lpYGcuo3C323ks2T3V8OXoDAkhjdkGf4uf3NuRiJFst0rgJejGSQNBcoNtQtSlnx
+    mzp7KzM0+R+u90bCglHGzZEZlah87UVt5188RQu/fRNuhuFA1wJbehLaRwARAQAB
+    tDJoZWxsb2xpbiAoaGVsbG9saW5sYXB0b3ApIDxoZWxsb2xpbm9pQG91dGxvb2su
+    Y29tPokCTgQTAQoAOBYhBAipLDN28HD9QhVgNG0nacBgiClHBQJlye1sAhsDBQsJ
+    CAcCBhUKCQgLAgQWAgMBAh4BAheAAAoJEG0nacBgiClHtg0P/2mT54fOqCghr0/y
+    RXkELKYoZNWbmUGqPJ1gVaT928H4/einkkTZMW86Z3B5HkO4aZhi7KEgi12655wU
+    uLuZngxN2PNw2yXxlOZJ2qKVuBajGobdlBRIUasXrn88PduJpXYMHldBaLdHpHm1
+    1qOvAkGfPRexV44vEOmgLD1JZEJPm4jVD/9l96YUqtaGaB7dQi2imt2+zL76xNq5
+    8cc1LkxE0UuGIy9POhhGgay6a3nxB4prGdowV2RzqBn3OMyyWGLn60fVjF4rO1cU
+    PEfkniJCUURIt6WXuhxZVEwpS0HhCqBbA5vY/ak/indFaLrH480Lbx6Kw2KmFQ4D
+    gbysUvY5P/vniU7zYuBJ3JhSzdx9HGLtEIzyksnttNW2wuI/4i9N8/jMNRpFn1c4
+    MXGWJLaDhHynJySM3R6W+56j1eLfM3sGSMHKhiVFewK3ePzPheHLibnxLhtd/nlq
+    USpW4gYriHA9JfeMQIlS/TJAC2SuaJC8fInqoIkCDvaT4czDBzrSZViL8c1u6QRA
+    iACJQAeHMQnYmA+9GoSmzRACLeRHC8+KIeqTszP8Jr7kE6I2ht+OmOCNlflCf/8o
+    PpyzCs6aZ8lVNTrBx6antRetrOyjqtXfdSQB5UCRRgKdpk5Clzn9SvS1ulfue2Mx
+    T97L9SJZogTcfpYiRtovFXgBObEeuQINBGXJ7WwBEADAXBQsVd1EPDIGZOxFwu4l
+    j1Vp5JT+mI6Vl4/SnjhOmF6gw/199wWsfvLtpFDTyDgE+MZFRu1EbXqlMOdSHIXE
+    6g7bIlCA3jG2A+IOLpxLMPCQEclbiIYWZPIFoA8agBf3wXwxmRfahCCKN2bh1Fnf
+    qoRjUoBH565DeHy+HMzhO9f4O6P+y2rh2WogL6FtP7N62xTJa+M2ZATRKvosRM6E
+    SFvnjnCrBHQwl6Aig33PvzFA+s6v15A1jOcgriQgxC/hWco9XZ1jucOc0aBz7akG
+    NaTXKBpXkD5dfnxHnQmwJGWb3XuIBpwF9niJydK8Ku5GGwGVJgplVqg+xNu3TyKL
+    wktm9vQHW2AamAzW+61EopI5tnNXWGKDrF8XA2PtVdZ70eDm/u7xCxpD5jL0Jwv0
+    3CKfXS/8o5RFwO35eKINAe3i3Ivb4IAun3rUp+tI2D5iTimKuzHAm8foNlGc5JoX
+    HyD5Pi0qHCNqZKa9+n3Ems49Q0oJj9cOb/px8YPqmZPYLsXxoVa/+k4E27ZcVOc5
+    MNLZse/yyPo1AHywZrHHBjGfmz23ETPeUN/qV29eePihBZKtge2PIFcan7XoswOC
+    xNXhMHWJTReuf98IgdQchpbhyAS0QeXDzSlKtwxU37ea1u5IU1Frf5aaZLvgv8NN
+    0PTWdYwfmVpUnN12ZjpPKQARAQABiQI2BBgBCgAgFiEECKksM3bwcP1CFWA0bSdp
+    wGCIKUcFAmXJ7WwCGwwACgkQbSdpwGCIKUcZMhAAocDTZ5+IqCVKNXntAsCrbM1e
+    Fi7xXTugKHYbCrtMmMGsIqqKm3vnnXItjlec3LZm9y4cA4ZbbQFZ4NeLglnSdaZP
+    aTSHDJqqeo1/XPW0YXIfbYLy9PhhoX8vQNfAWVYck2kObjHqqTzyPO1p9hvEF8+Y
+    X3b8vbAAIp0jJihXGmKzKQDl+W5IkORmdG4U8fR77WUOY+lZj3eDFpZA4VcaDewG
+    nappePNRjRhTKs7um2TOmFiZD8Fefu4jmPFOG2bnJFNg6o4mzmpcq/oQ+irJ99jH
+    3jzKiKr6z2Fj0VyEV2b/EB9lVFJjhoyegQLqDiIdO1TbX1gXrcrVwC+Nymmmu5Ux
+    O4RGalcsPwSzOn6/yU8YBZbqlgSXakT1MmmHDorUD/thFqm+mI6pWhu81zg7WjZw
+    lkfJeqI0j1M330Z3yfWw4o+Ypk8ugRna194/jNh9eQoGwCH62cs9dQpGFY4cNfKB
+    XtPrmHFcmCPbln9qtwy00UHHcSFiA5m98qj652VR9BjStX9hCpupOR1DQkXMzLWC
+    r/5/8cnF/i4VkrRDpV4RQN7j31dotXesswrZd3B2LO3wfyGDJvhW55A0XMgYzu5M
+    JyUt4g4u1Zdtys67PwuWBizJmUyuq/Vqcez3hly22dBN8Ms+1EqFmCSfI4OCN3FG
+    mMNH+TycbmC2wD08XOQ=
+    =dG5v
+    -----END PGP PUBLIC KEY BLOCK-----
 #bg
 .container
   #left
@@ -43,7 +97,7 @@ setColorScheme('#656d88');
           mdui-list-item(description='Always learning!' icon='language' rounded) zh-CN (Native) / en-US / ja-JP
           mdui-list-item(description='Most used languages' icon='computer' rounded) C++, TypeScript, C#, Kotlin
           mdui-list-item(description='Click to email me' icon='email' rounded href='mailto://hellolinoi@outlook.com') hellolinoi@outlook.com
-          mdui-list-item(description='Click to view my PGP Public Key' icon='key' rounded href='https://github.com/hellolin-oi.gpg' target='_blank') Key ID: 6D27 69C0 6088 2947
+          mdui-list-item(description='Click to view my PGP Public Key' icon='key' rounded href='javascript:document.querySelector(".pgp_dialog").open=true') Key ID: 6D27 69C0 6088 2947
       .society
         h2 Contact me
         mdui-list
